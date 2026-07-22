1481b868927350dc84639b0aa3be14edc046a9f1
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-23 04:01:15 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+BOT_TOKEN = os.getenv(
+    "BOT_TOKEN"
+)
+
+
+AI_ASSISTANT_URL = (
+    "https://t.me/Intradayxauusd_bot"
+)
+
+
+AI_COMMUNITY_URL = (
+    "https://t.me/AITOOLSIGNAL_BOT"
+)
